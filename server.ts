231f26fb5b2176b37dc361dc3a776819369df2df
sushil <sushilgarbuja82335@gmail.com>
2025-03-12 22:55:31 +0545

    first commit

---
+++ server.ts
@@ -0,0 +1,11 @@
+import app from "./src/app";
+import { envConfig } from "./src/config/config";
+
+
+function startServer(){
+    const port=envConfig.port || 5000
+    app.listen(port,()=>{
+        console.log(`Server is running on port [${port}]`);
+    })
+}
+startServer();
