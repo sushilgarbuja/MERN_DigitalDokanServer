37b63074d70c6c2eb9884827bda68d2fe4a67a8a
sushil <sushilgarbuja82335@gmail.com>
2025-07-02 08:12:41 +0545

    day 59

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import { OrderStatus } from './src/globals/types/index';
 import adminSeeder from "./adminSeeder";
 import app from "./src/app";
 import { envConfig } from "./src/config/config";
@@ -5,6 +6,7 @@ import categoryController from "./src/controllers/categoryController";
 import { Server } from 'socket.io';
 import jwt from 'jsonwebtoken';
 import User from "./src/database/models/userModel";
+import Order from "./src/database/models/orderModel";
 
 function startServer() {
     const port = envConfig.port || 3000;
@@ -28,23 +30,42 @@ function startServer() {
     };
 
     io.on('connection', (socket) => {
-        const { token } = socket.handshake.auth;
+        const  token  = socket.handshake.headers.token;
         if (token) {
-            jwt.verify(token, envConfig.jwtSecret as string, async (err:any, result: any) => {
+            console.log(token);
+            jwt.verify(token as string, envConfig.jwtSecret as string, async (err:any, result: any) => {
                 if (err) {
-                    socket.emit("error", err.message);
+                    socket.emit("error", err);
                 } else {
-                    console.log(result);
-                    const userData = await User.findByPk(result?.userId); // {email}
+                    const userData = await User.findByPk(result.userId);
+                    console.log(userData); 
                     if (!userData) {
                         socket.emit("error", "User not found");
                         return;
                     }
                     // Correctly pass userId to addToOnlineUsers
                     addToOnlineUsers(socket.id, result.userId, userData.role);
+                    console.log(onlineUsers);
                 }
-            });
+            })
+        }else{
+            socket.emit("error","Token not found")
         }
+        socket.on("updateOrderStatus", async(data)=>{
+            const{status,orderId,userId}=data;
+            console.log("updateOrderStatus",data);
+
+           const findUser=onlineUsers.find(user=>user.userId==userId)
+           await Order.update({orderStatus:status},{where:{id:orderId}})
+           
+           if(findUser){
+           console.log(findUser);
+
+            io.to(findUser.socketId).emit("success","Order status updated",data);
+           }else{
+            socket.emit("error","User is not online")
+           }
+        })  
     });
 }
 
